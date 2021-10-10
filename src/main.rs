d3f74aced502bce9be054f67548e37419e71b42d
Jim Hessin <jhessin@gmail.com>
2021-10-09 20:17:48 -0600

    Reformatted yml to be parsed properly.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use crate::lib::workout::{DayOfWeek, Workout, WorkoutType};
-
-use std::collections::HashMap;
 use std::error::Error;
 
 mod lib;
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let monday = Workout::load_file("data/3-1-LBA.yml")?;
+  let workouts = vec![
+    Workout::load_file("data/3-1-LBA.yml")?,
+    Workout::load_file("data/3-2-UBA.yml")?,
+    Workout::load_file("data/3-3-LBA.yml")?,
+    Workout::load_file("data/3-4-UBA.yml")?,
+  ];
 
-  println!("workout = {:?}", monday);
+  println!("workouts = {:#?}", workouts);
   Ok(())
 }
